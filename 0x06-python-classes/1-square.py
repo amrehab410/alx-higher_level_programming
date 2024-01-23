31f7a93a1b30078717acd65c296ab8044cfad635
root <root@DESKTOP-LP5R8B0.localdomain>
2024-01-23 20:40:20 +0200

    class1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,5 +8,10 @@ class Square:
     """
     Represent a square
     """
-    def __init__(size):
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size

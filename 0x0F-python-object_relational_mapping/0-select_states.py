166c308a5a3bcb3ec0b42620aa7211ba73bc0075
root <root@DESKTOP-LP5R8B0.localdomain>
2024-04-15 16:48:26 +0200

    orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states;")
-states = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
 
-for state in states:
-    print(state)
+    for state in states:
+        print(state)
